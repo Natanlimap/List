d9766f38517c1ff4754acc7dbf6f4d1db7abd7ab
Natanlimap <natanlimap@gmail.com>
2019-10-25 20:50:06 -0300

    Operator == and != done

--- test/test.cpp
+++ test/test.cpp
@@ -7,18 +7,18 @@
 
 int main(){
 	sc::list<int> L{1,2,3,4,5,6};
-	sc::list<int>::iterator it = L.end();
-	L.push_front(9);
-	L.push_back(10);
+	sc::list<int>G(10);
+
+	std::cout<<"G VVVVV"<<std::endl;
+	G.print();
+	std::cout<<"L VVVVV"<<std::endl;
 	L.print();
-	L.pop_front();
-	L.print();
-	L.pop_back();
-	*L.end() = 100;
-	L.print();
-	std::cout<<"FRONT ="<<L.front() << "||BACK = "<<L.back() << std::endl;
-	L.assign(7);
-	L.print();	
-	L.clear();
+	assert((G != L));
+
+	G = L;
+	assert(G == L);
+	std::cout<<"G VVVVV"<<std::endl;
+	G.print();
+	std::cout<<"L VVVVV"<<std::endl;
 	L.print();
 }
